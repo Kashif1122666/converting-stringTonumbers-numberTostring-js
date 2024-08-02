014a899a98a3ca13e01aa8214b4e8c85a7597457
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-08-02 15:42:09 -0700

    Add files via upload
    done

---
+++ index.js
@@ -0,0 +1,27 @@
+// Chapter-29: converting strings to nunbers, nunbers to strings
+
+// CONVERTING STRING TO NUMBER
+// number with string type
+// let numString = "25"
+
+// checking type using typeof operator
+// console.log(typeof numString);
+
+// converting string to number
+// let convertedNumber  = Number(numString);
+
+// checking type using typeof operator
+// console.log( typeof convertedNumber);
+
+// CONVERTING NUMBER TO STRING
+
+// let givenNumber = 35;
+
+// checking type using typeof operator
+// console.log( typeof givenNumber);
+
+// converting  number to string
+// let convertedString = givenNumber.toString(); 
+
+// checking type using typeof operator
+// console.log( typeof convertedString);
